485e8c5f7ff5d34b61901e88b5e22cd31c09620b
nick3fc <nick3fc@gmail.com>
2026-05-19 21:38:03 +0300

    hw08_first_release

--- js/task-3.js
+++
@@ -1,9 +0,0 @@
-const input = document.querySelector('#name-input');
-const output = document.querySelector('#name-output');
-
-input.addEventListener('input', onInputChange);
-
-function onInputChange(event) {
-  const inputValue = event.target.value.trim();
-  output.textContent = inputValue || 'Anonymous';
-}
